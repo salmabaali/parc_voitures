ba9bf613f4f47a7a1c669898360616726536d193
salma <selmabaali20@gmail.com>
2026-03-08 18:34:11 -0400

    Creation de la classe Voiture

---
+++ voitures.py
@@ -0,0 +1,6 @@
+class Voiture:
+    def __init__(self, marque, modele, immatriculation):
+        self.marque = marque
+        self.modele = modele
+        self.immatriculation = immatriculation
+        
