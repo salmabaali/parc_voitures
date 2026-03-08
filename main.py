ea823b4f7fcc551087970402537f0bf1c0173a8b
salma <selmabaali20@gmail.com>
2026-03-08 18:46:01 -0400

    Creation du programme principal

---
+++ main.py
@@ -0,0 +1,9 @@
+from voitures import Voitures
+from parc import Parc
+mon_parc = Parc(3)
+voiture1 = Voitures("Toyota", "Corolla", "ABC13")
+voiture2 = Voitures("Honda", "Civic", "XYZ789")
+voiture3 = Voitures("kia", "Focus", "LMN456")
+voiture4 = Voitures("Tesla", "Model 3", "TES123")
+mon_parc.sortirVoiture(voiture2)
+print("Places libres :", mon_parc.calculerNbrPlacesLibres())
